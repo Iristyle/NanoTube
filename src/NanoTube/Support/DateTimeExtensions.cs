96c02d51b8294c3a581348906b3c157e4afd437b
Ethan J. Brown <ethan.j.brown@gmail.com>
2012-01-20 10:57:27 -0500

    changed namesapce and wrote docs

--- src/NanoTube/Support/DateTimeExtensions.cs
+++ src/NanoTube/Support/DateTimeExtensions.cs
@@ -1,19 +1,23 @@
-﻿namespace NanoTube
+﻿namespace NanoTube.Support
 {
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 
 	/// <summary>
-	/// TODO: Update summary.
+	/// Provides a simple set of extension on DateTime.
 	/// </summary>
 	public static class DateTimeExtensions
 	{
-		private static DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+		private readonly static DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
+		/// <summary>	Returns the DateTime as the number of seconds since the epoch (1970), which is Unix time. </summary>
+		/// <remarks>	1/20/2012. </remarks>
+		/// <param name="dateTime">	The dateTime to act on. </param>
+		/// <returns>	A number of seconds since the epoch. </returns>
 		public static double AsUnixTime(this DateTime dateTime)
 		{
-			return Math.Round(dateTime.ToUniversalTime().Subtract(epoch).TotalSeconds);
+			return Math.Round(dateTime.ToUniversalTime().Subtract(_epoch).TotalSeconds);
 		}
 	}
 }
