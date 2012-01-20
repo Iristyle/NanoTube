582599e3f2e96bdc1fcadc144f0a12d705e7459c
Ethan J. Brown <ethan.j.brown@gmail.com>
2012-01-20 10:52:26 -0500

    wrote missing docs

--- src/NanoTube/Linq/LinqExtensions.cs
+++ src/NanoTube/Linq/LinqExtensions.cs
@@ -3,13 +3,17 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
-	using System.Text;
 
 	/// <summary>
 	/// TODO: Update summary.
 	/// </summary>
 	public static class LinqExtensions
 	{
+		/// <summary>	Breaks a given enumeration into specific batch sizes.. </summary>
+		/// <typeparam name="T">	The Type of the enumeration. </typeparam>
+		/// <param name="list">			The list to act on. </param>
+		/// <param name="batchSize">	Size of the batch. </param>
+		/// <returns>	Will create lazy collections of the given batch size as items are read from the IEnumerable. </returns>
 		public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> list, int batchSize)
 		{
 			var batch = new List<T>(batchSize);
--- src/NanoTube/Net/PacketBuilder.cs
+++ src/NanoTube/Net/PacketBuilder.cs
@@ -5,15 +5,28 @@ namespace NanoTube.Net
 	using System.Linq;
 	using System.Text;
 
+	/// <summary>	A helper class for turning a list of strings into a Udp packet.  </summary>
+	/// <remarks>	1/19/2012. </remarks>
 	public static class PacketBuilder
 	{
 		private static byte[] _terminator = Encoding.UTF8.GetBytes("\n");
 
+		/// <summary>
+		/// Takes a list of metric strings, separating them with newlines into a byte packet that is a maximum of 512 bytes in size.
+		/// </summary>
+		/// <param name="metrics">	The metrics to act on. </param>
+		/// <returns>	A streamed list of byte arrays, where each array is a maximum of 512 bytes. </returns>
 		public static IEnumerable<byte[]> ToMaximumBytePackets(this IEnumerable<string> metrics)
 		{
 			return ToMaximumBytePackets(metrics, 512);
 		}
 
+		/// <summary>
+		/// Takes a list of metric strings, separating them with newlines into a byte packet of the maximum specified size.
+		/// </summary>
+		/// <param name="metrics">   	The metrics to act on. </param>
+		/// <param name="packetSize">	Maximum size of each packet (512 bytes recommended for Udp). </param>
+		/// <returns>	A streamed list of byte arrays, where each array is a maximum of 512 bytes. </returns>
 		public static IEnumerable<byte[]> ToMaximumBytePackets(this IEnumerable<string> metrics, int packetSize)
 		{
 			List<byte> packet = new List<byte>(packetSize);
