e2d78eed52f2ccd6d74ad4b747e5cd9c443aa08d
Ethan J. Brown <ethan.j.brown@gmail.com>
2012-01-20 11:02:19 -0500

    made a number of changes to the UdpMessenger class to support varied scenarios
    - HealConnection not needed since the RemoteEndPoint info is established at send time
    - instead of caching Dns query results for what could be an infinite run, create new IPEndPoint instances per iteration -- this ensures that if Dns changes while the application is running, its not using the wrong IP
    - Created a new StreamMetrics method which can optionally accept a number of packets to send at a time, where the default is 10.
    - Semantics of SendMetrics vs StreamMetrics annotated in docs -- StreamMetrics will break down the IEnumerable into bite size chunks as it goes whereas SendMetrics will materialize the whole IEnumerable first
    - Fleshed out all documentation

--- src/NanoTube/Net/UdpMessenger.cs
+++ src/NanoTube/Net/UdpMessenger.cs
@@ -6,18 +6,20 @@
 	using System.Net.Sockets;
 	using System.Net;
 	using Collections;
+	using Linq;
 
 	/// <summary>
-	/// TODO: Update summary.
+	/// A class that performs Udp communications with a remote host.  It can either Send metrics data all in one shot, or it can stream a
+	/// specific number of packets at a time over an infinite IEnumerable.
 	/// </summary>
+	/// <remarks>	1/19/2012. </remarks>
 	public class UdpMessenger : IDisposable
-	{
+	{		
 		private readonly static SimpleObjectPool<SocketAsyncEventArgs> _eventArgsPool 
 			= new SimpleObjectPool<SocketAsyncEventArgs>(30, pool => new PoolAwareSocketAsyncEventArgs(pool));
-		private readonly string _hostname;
 		private readonly int _port;
+		private readonly string _hostname;
 		private readonly UdpClient _client;
-		private readonly IPEndPoint _endpoint;
 		private bool _disposed;
 
 		/// <summary>
@@ -30,12 +32,10 @@
 			_hostname = hostname;
 			_port = port;
 			_client = new UdpClient();
-			_endpoint = new IPEndPoint(Dns.GetHostEntry(hostname).AddressList[0], port);
 			_client.Client.SendBufferSize = 0;
 		}
 
 		/// <summary>	Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources. </summary>
-		/// <remarks>	12/28/2011. </remarks>
 		public void Dispose()
 		{
 			if (!this._disposed)
@@ -46,7 +46,6 @@
 		}
 
 		/// <summary>	Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources. </summary>
-		/// <remarks>	12/28/2011. </remarks>
 		/// <param name="disposing">	true if resources should be disposed, false if not. </param>
 		protected virtual void Dispose(bool disposing)
 		{
@@ -60,17 +59,13 @@
 			}
 		}
 
-		private void HealConnection()
-		{
-			//heal connection if necessary
-			if (!_client.Client.Connected)
-			{
-				try { _client.Connect(_hostname, _port); }
-				catch { }
-			}
-		}
-
-		//TODO: write a separate version that instead processes each packet individually inside the loop
+		/// <summary>	Sends all of the given metrics in the IEnumerable. </summary>
+		/// <remarks>
+		/// It's critical to realize that this method will materialize the entire set of metrics up-front, and then return. This method is *not*
+		/// not appropriate for use in streaming from an infinite IEnumerable, unless the stream is first batched up using .Chunk().  Use
+		/// StreamMetrics to let the messenger Chunk() on your behalf.
+		/// </remarks>
+		/// <param name="metrics">	The metrics. </param>
 		public void SendMetrics(IEnumerable<string> metrics)
 		{
 			var data = _eventArgsPool.Pop();
@@ -79,15 +74,11 @@
 
 			try
 			{
-				data.RemoteEndPoint = _endpoint;
+				data.RemoteEndPoint = new IPEndPoint(Dns.GetHostAddresses(_hostname)[0], _port);
 				data.SendPacketsElements = metrics.ToMaximumBytePackets()
 					.Select(bytes => new SendPacketsElement(bytes, 0, bytes.Length, true))
 					.ToArray();
 
-				HealConnection();
-
-				//SendAsync is superior to BeginSend / EndSend
-				//_client.Client.SendAsync(eventArgs);
 				//_client.Client.NoDelay = true;
 				_client.Client.SendPacketsAsync(data);
 
@@ -96,6 +87,53 @@
 			//fire and forget, so just eat intermittent failures / exceptions
 			catch
 			{ }
+		}
+
+		/// <summary>	Streams the given metrics in the IEnumerable, terminating when the IEnumerable does. </summary>
+		/// <remarks>
+		/// This method will not materialize the entire set of metrics up-front, but will instead fill up to the full 512 bytes of a UDP packet,
+		/// and will create 10 packets before sending.  It can run infinitely if the IEnumerable passed in is infinite. If there are no available
+		/// SocketAsyncEventArgs in the pool, it will discard the current number of specified packets. This method is *not* appropriate if all
+		/// metrics should be sent immediately in one shot. Use SendMetrics for that use case.
+		/// </remarks>
+		/// <param name="metrics">	The metrics. </param>
+		public void StreamMetrics(IEnumerable<string> metrics)
+		{
+			StreamMetrics(metrics, 10);
+		}
+
+		/// <summary>	Streams the given metrics in the IEnumerable, terminating when the IEnumerable does. </summary>
+		/// <remarks>
+		/// This method will not materialize the entire set of metrics up-front, but will instead fill up to the full 512 bytes of a UDP packet,
+		/// and will create the given number of packets specified before sending.  It can run infinitely if the IEnumerable passed in is
+		/// infinite. If there are no available SocketAsyncEventArgs in the pool, it will discard the current number of specified packets. This
+		/// method is *not* appropriate if all metrics should be sent immediately in one shot. Use SendMetrics for that use case.
+		/// </remarks>
+		/// <param name="metrics">		 	The metrics. </param>
+		/// <param name="packetsPerSend">	Size of the chunk. </param>
+		public void StreamMetrics(IEnumerable<string> metrics, int packetsPerSend)
+		{
+			foreach (var chunk in metrics.ToMaximumBytePackets().Chunk(packetsPerSend))
+			{
+				var data = _eventArgsPool.Pop();
+				//firehose alert! -- keep it moving!
+				if (null == data) { continue; }
+
+				try
+				{
+					data.RemoteEndPoint = new IPEndPoint(Dns.GetHostAddresses(_hostname)[0], _port);
+					data.SendPacketsElements = chunk
+						.Select(bytes => new SendPacketsElement(bytes, 0, bytes.Length, true))
+						.ToArray();
+
+					_client.Client.SendPacketsAsync(data);
+
+					//Write-Debug "Wrote $(byteBlock.length) bytes to $server:$port"
+				}
+				//fire and forget, so just eat intermittent failures / exceptions
+				catch
+				{ }
+			}
 		}
 	}
 }
